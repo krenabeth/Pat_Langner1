054cde304d015d40327f4002c9dabb40f7b35e0e
Karen Langner <krenabeth@yahoo.com>
2016-07-06 16:06:31 -0400

    navigation bar, styling fixed, background image

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById("nav01").innerHTML =
+	"<ul id='navBar'>" +
+		"<li><a href='index.html'>Home</a></li>" +
+		"<li><a href='about.html'>About</a></li>" +
+		"<li><a href='#'>Gallery</a></li>" +
+		"<li><a href='#'>Shows</a></li>" +
+		"<li><a href='#'>Shop</a></li>" +
+	"</ul>";
