008fd988a4af272eef6dae3340e19c793daff7d2
Karen Langner <krenabeth@yahoo.com>
2016-07-24 20:01:25 -0400

    fixed routing

--- js/routeController.js
+++ js/routeController.js
@@ -1,5 +1,31 @@
-var app = angular.module('routingModule');
+var app = angular.module('routingModule', ['ngRoute']);
 
-app.controller("routeCtrl", function() {
-
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/',
+	{
+		templateUrl: '/',
+		controller: 'routeCtrl'	
+	})
+	.when ('/html/shows',
+	{
+		templateUrl: 'html/shows.html',
+		controller: 'routeCtrl'
+	})
+	.when ('/html/about',
+	{
+		templateUrl: 'html/about.html',
+		controller: 'routeCtrl'   
+	})
+	.when ('/html/shop',
+	{
+		templateUrl: 'html/shop.html',
+		controller: 'routeCtrl'   
+	})
+	.when ('/html/gallery',
+	{
+		templateUrl: 'html/gallery.html',
+		controller: 'routeCtrl'   
+	})
+	.otherwise({ redirectTo: '/'})
 });
