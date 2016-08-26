66ce5152c641e771486b3417e0c891089ccfa806
Karen Langner <krenabeth@yahoo.com>
2016-08-25 23:28:13 -0400

    styling on contact form and nav, images on shows view

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 var app = angular.module('routingModule');
+
+// app.controller('routeCtrl', function($scope, $location) {
+//     $scope.isActive = function(route) {
+//         return route === $location.path();
+//     }
+// });
